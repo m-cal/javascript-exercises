22dafd0d71b3619cafa8a867ddae7e2893a788b3
m-cal <ctrlshiftpjustdoit@gmail.com>
2022-10-19 18:49:14 -0400

    Complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(str, n) {
+  let toReturn = '';
+  for (i = 0; i < n; i++) {
+    toReturn += str;
+  }
+  return toReturn;
 };
 
 // Do not edit below this line
