93eb00d3d1507a445c9d19746f4412547b085ef6
m-cal <ctrlshiftpjustdoit@gmail.com>
2022-10-20 00:47:35 -0400

    complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,23 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...theRest) {
+  const toReturn = [];
 
-};
+  arr.forEach(elem => {
+    if (!theRest.includes(elem)) {
+      toReturn.push(elem);
+    }
+  });
+  return toReturn;
+}
+
+/* 
+
+loop through each elem of arr
+at each elem, loop through ...theRest
+if there is a match, remove the original from arr
+
+
+*/
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
