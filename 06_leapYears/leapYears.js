68d3d6ea48aeb0cb4147768f5d153f07f4957d9d
m-cal <ctrlshiftpjustdoit@gmail.com>
2022-10-20 01:01:03 -0400

    Solve 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,29 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+  if ((year % 100 === 0) && year % 400 !== 0) {
+    return false;
+  } else if (year % 4 === 0) {
+    return true;
+  } else {
+    return false;
+  }
 };
+
+
+/* 
+TRUE:
+  % 4 == 0
+UNLESS
+  % 100 == 0
+  ||
+  %
+
+*/
+
+
+
+
+
+
 
 // Do not edit below this line
 module.exports = leapYears;
