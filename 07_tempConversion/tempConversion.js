71c02f22251fd63e621bcd704a45ee2590ca7669
m-cal <ctrlshiftpjustdoit@gmail.com>
2022-10-20 02:46:22 -0400

    Solve 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,12 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  if (f == 32) {
+    return 0
+  } 
+  return Number((((f-32)*5)/9).toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  return Number(((c * 1.8) + 32).toFixed(1));
 };
 
 // Do not edit below this line
