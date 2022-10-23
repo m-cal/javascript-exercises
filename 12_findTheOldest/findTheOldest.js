388c566cb72420ebc685ed410d1cc3b5728fefed
m-cal <ctrlshiftpjustdoit@gmail.com>
2022-10-23 11:46:43 -0400

    Solve 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,30 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  const ages = [];
+  console.log(ages);
+  for (let person = 0; person < people.length; person++) {
+    if (!people[person].yearOfDeath) {
+      ages.push((new Date().getFullYear()) - people[person].yearOfBirth);
+    } else {
+      ages.push(people[person].yearOfDeath - people[person].yearOfBirth);
+    }
+  }
+  console.log(ages);
+  ages.forEach(age => {
+    if (typeof age != 'number') {
+      ages.splice(age, 1);
+    }
+  });
+  console.log(ages);
+  const agesArr =  ages.sort((a, b) => {return a - b});
+  const oldest = agesArr.reverse()[0];
+  const oldestPerson = people.find((person) => {
+    if (!person.yearOfDeath) {
+     return ((new Date().getFullYear()) - person.yearOfBirth) == oldest;
+    } else { 
+      return person.yearOfDeath - person.yearOfBirth == oldest;
+    }
+  });
+  return oldestPerson;
 };
 
 // Do not edit below this line
