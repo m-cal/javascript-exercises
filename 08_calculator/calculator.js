130ed62d15ec38ad6efcff3a573c70ee8664d555
m-cal <ctrlshiftpjustdoit@gmail.com>
2022-10-22 21:30:22 -0400

    Complete add, sub, sum, mult

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,17 +1,35 @@
-const add = function() {
-	
+const add = function(...nums) {
+  let sum = 0;
+  for (let num of nums) {
+    sum += num;
+  }
+  return sum;
+}
+
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const subtract = function() {
-	
+const sum = function(arr) {
+	if (arr.length == 0) {
+    return 0;
+  } else if (arr.length == 1) {
+    return arr[0];
+  } else {
+    let total = 0;
+    for (let i = 0; i < arr.length; i++) {
+      total += arr[i];
+    }
+    return total;
+  }
 };
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
+const multiply = function(numArr) {
+  let total = 1;
+  for (let num of numArr) {
+    total *= num;
+  }
+  return total;
 };
 
 const power = function() {
