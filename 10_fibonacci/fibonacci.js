fdfe5eb2364c9dabedf7e25031ea3e7c2f467f08
m-cal <ctrlshiftpjustdoit@gmail.com>
2022-10-22 23:20:08 -0400

    Solve 10_fibonnaci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(nthFib) {
+  if (typeof nthFib === 'string') {
+    nthFib = Number(nthFib);
+  }
+  if (nthFib < 0) {
+    return 'OOPS';
+  }
+  if (nthFib == 1 || nthFib == 2) {
+    return 1;
+  }
+  let arr = [1, 1, 2];
+  for (let currentFibDigits = 3; currentFibDigits < nthFib; currentFibDigits++) {
+    arr.push((arr[arr.length-1] + arr[arr.length-2]));
+  }
+  return arr[arr.length-1];
 };
 
 // Do not edit below this line
