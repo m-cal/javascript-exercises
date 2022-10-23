796da76f4294777846898ce4edccf06f079cc51b
m-cal <ctrlshiftpjustdoit@gmail.com>
2022-10-22 22:43:58 -0400

    Solve 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let comparator1 = str.split(' ').join('').toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+  console.log(comparator1);
+  let comparator = str.split(' ').join('').split('').reverse().join().toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+  console.log(comparator);
+  if (comparator1 == comparator) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
