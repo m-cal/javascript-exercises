e945280907999fd643b47938b63165135865272e
m-cal <ctrlshiftpjustdoit@gmail.com>
2022-10-23 10:19:11 -0400

    Solve 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,10 @@
-const getTheTitles = function() {
-
-};
+const getTheTitles = function(books) {
+  const toReturn = [];
+  for (const book of books) {
+    toReturn.push(book.title);
+  }
+  return toReturn;
+}
 
 // Do not edit below this line
 module.exports = getTheTitles;
